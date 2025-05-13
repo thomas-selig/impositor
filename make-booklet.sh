b748a8b2abbb53dfcac0a2a18f0ac35cdff124ab
Thomas Selig <thomas@modio.se>
2025-05-13 15:49:12 +0200

    Clean-up of temporay and partial files

--- make-booklet.sh
+++ make-booklet.sh
@@ -5,6 +5,10 @@
 INPUT="$1"
 PRESET="${2:-org}"
 BASENAME=$(basename "$INPUT" .pdf)
+PAPER_WIDTH=297
+PAPER_HEIGHT=210
+declare -a TO_CLEAN=()
+
 # DIR=$(dirname "$(realpath "$INPUT")")
 DIR=$(dirname "$INPUT")
 
@@ -13,11 +17,14 @@ DIR=$(dirname "$INPUT")
 CROPPED="${DIR}/${BASENAME}-cropped.pdf"
 echo "Cropping whitespace..."
 pdfcrop "$INPUT" "$CROPPED" --resolution 72
+TO_CLEAN+=( "$CROPPED" )
 
 # Apply margin and set page size
 ########################################
 FIXED="${DIR}/${BASENAME}-fixed.pdf"
 SCALED="${DIR}/${BASENAME}-scaled.pdf"
+TO_CLEAN+=( "$FIXED" )
+TO_CLEAN+=( "$SCALED" )
 
 echo "Formatting to fixed size (preset: $PRESET)..."
 
@@ -26,7 +33,7 @@ case "$PRESET" in
     PAGE_WIDTH="95"
     PAGE_HEIGHT="148"
     INSET="2.5"
-    BINDING_EDGE="5"
+    BINDING_EDGE="8"
     ;;
   a5)
     PAGE_WIDTH="148"
@@ -52,10 +59,34 @@ pdfjam "$SCALED" --papersize "{${PAGE_WIDTH}mm,${PAGE_HEIGHT}mm}" --twoside  --s
 # Create booklet
 ################
 BOOKLET="${DIR}/${BASENAME}-book.pdf"
+TO_CLEAN+=( "$BOOKLET" )
   
 pdfbook --short-edge --scale 1 --noautoscale true --delta "${BINDING_EDGE}mm 0mm" "$FIXED" --outfile "$BOOKLET"
     
 echo "✅ Created booklet → $BOOKLET"
+
+# Create PDF with cut marks
+###########################
+pdflatex -interaction=nonstopmode -output-directory=./src \
+  "\
+\def\pw{$PAGE_WIDTH}\
+\def\ph{$PAGE_HEIGHT}\
+\def\m{5}\
+\def\sheetW{$PAPER_WIDTH}\
+\def\sheetH{$PAPER_HEIGHT}\
+\input{src/overlay.tex}"
+
+TO_CLEAN+=( "./src/overlay.aux" )
+TO_CLEAN+=( "./src/overlay.log" )
+TO_CLEAN+=( "./src/overlay.pdf" )
+
+# Overlay cut marks
+###################
+BOOKLET_CUTMARKS="${DIR}/${BASENAME}-book-cutmarks.pdf"
+echo "### BOOKLET_CUTMARKS=${BOOKLET_CUTMARKS}"
+echo "### BOOKLET=${BOOKLET}"
+pdftk "${BOOKLET}" stamp "./src/overlay.pdf" output "${BOOKLET_CUTMARKS}" 
+TO_CLEAN+=( "$BOOKLET_CUTMARKS" )
 
 # Seperate for duplex printing
 ##############################
@@ -63,15 +94,25 @@ echo "🖨️ Creating files for manual duplex printing..."
 
 EVEN="${DIR}/${BASENAME}-duplex1.pdf"
 ODD="${DIR}/${BASENAME}-duplex2.pdf"
-
 EVEN_TMP="${DIR}/${BASENAME}-even-tmp.pdf"
 
-gs -sDEVICE=pdfwrite -sPageList=odd -sOutputFile="$ODD" -dBATCH -dNOPAUSE -dAutoRotatePages=/None "$BOOKLET"
+TO_CLEAN+=( "$EVEN_TMP" )
 
-gs -sDEVICE=pdfwrite -sPageList=even -sOutputFile="$EVEN_TMP" -dBATCH -dNOPAUSE -dAutoRotatePages=/None "$BOOKLET"
+gs -sDEVICE=pdfwrite -sPageList=odd -sOutputFile="$ODD" -dBATCH -dNOPAUSE -dAutoRotatePages=/None "$BOOKLET_CUTMARKS"
+
+gs -sDEVICE=pdfwrite -sPageList=even -sOutputFile="$EVEN_TMP" -dBATCH -dNOPAUSE -dAutoRotatePages=/None "$BOOKLET_CUTMARKS"
 
 pdfjam "$EVEN_TMP" 'last-1' --outfile "$EVEN" --landscape --scale 1 --noautoscale true 
 
 echo "✅ Manual duplex files:"
 echo "→ Front side: $ODD"
 echo "→ Back side:  $EVEN"
+
+TO_CLEAN+=( "$CROPPED" )
+
+# Clean up
+##########
+if (( ${#TO_CLEAN[@]} )); then
+  echo "Cleaning up ${#TO_CLEAN[@]} temporary files…"
+  rm -f "${TO_CLEAN[@]}"
+fi
